e4b61061f814ac071db6ef1aa0d5b71614fc7a0f
Johnson, Joshua A <163660@s020rmc148-04.local>
2018-11-14 13:36:06 -0600

    I am now about to learn about branches within the repostiory

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Johnson, Joshua A on 11/14/18.
+//  Copyright © 2018 Johnson, Joshua A. All rights reserved.
+//
+
+import Foundation
